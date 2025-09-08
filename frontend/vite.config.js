ee597432e167f6b30c9ac36c550603e6af42c635
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-08 12:10:12 -0300

    all: rig a new dockerfile for frontend and fix issues with fucking vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), tailwindcss()],
+  server: {
+    host: true,                 // listen on 0.0.0.0
+    allowedHosts: ['frontend'], // allow Docker service name
+  },
 })
