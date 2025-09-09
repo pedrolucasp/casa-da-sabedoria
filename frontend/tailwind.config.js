952a6fb9b7ea0560f53542ba7c5f6f5949d465ca
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-09 01:32:40 -0300

    frontend: add tooling for components and slight tweakings to the layout

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+// XXX: I think this won't work for the new tailwind?
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        mono: ["Menlo", "monospace"], // TODO: Replace with something else
+      },
+    },
+  },
+}
+
