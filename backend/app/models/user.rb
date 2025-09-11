5bc593c9b9077254cf94c0647635e470f514a32e
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-11 12:05:34 -0300

    backend: very basic user model & devise install

---
+++ backend/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  validates :name, presence: true
+end
