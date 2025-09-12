107efe40b50f7358c57b535efa746cc535ac8c4e
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-12 16:00:31 -0300

    frontend: add biome as a dependency for integrated linter/fixer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+import biomePlugin from 'vite-plugin-biome';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [preact(), tailwindcss(), biomePlugin()],
   server: {
     host: true,                 // listen on 0.0.0.0
     allowedHosts: ['frontend'], // allow Docker service name
