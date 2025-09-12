33bd9317226bca1cb1004154f110542b1113d891
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-12 17:18:24 -0300

    backend: flesh out tests for auth & user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
     :recoverable,
     :rememberable,
     :validatable,
-    :confirmable,
     :trackable,
     :jwt_authenticatable,
     jwt_revocation_strategy: JwtDenylist
+  # :confirmable,
 
   validates :name, presence: true
 end
