bbeb71d26f1f9f12a6c14c3fd265bc99bd21625d
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-14 20:51:25 -0300

    frontend: add h3 and allow custom classes for typography

--- frontend/src/components/Typography.js
+++ frontend/src/components/Typography.js
@@ -13,10 +13,14 @@ export const H1 = ({ children, variant = "default", className, ...props }) => {
   )
 }
 
-export const H2 = ({ children }) => (
-  <h2 className="text-xl font-semibold text-neutral-900">{children}</h2>
+export const H2 = ({ children, className }) => (
+  <h2 className={cn("text-xl font-semibold text-neutral-900", className)}>{children}</h2>
 )
 
-export const P = ({ children }) => (
-  <p className="text-sm text-neutral-600">{children}</p>
+export const H3 = ({ children, className }) => (
+  <h3 className={cn("text-md font-semibold text-neutral-900", className)}>{children}</h3>
+)
+
+export const P = ({ children, className }) => (
+  <p className={cn("text-sm text-neutral-600", className)}>{children}</p>
 )
