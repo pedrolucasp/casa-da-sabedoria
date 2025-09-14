d61b8265fd49854e83180253257a84a817163b94
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-14 20:52:08 -0300

    frontend: create a book form

--- frontend/src/routes/books/new.js
+++ frontend/src/routes/books/new.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from "preact/hooks";
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { Button } from '../../components/Button';
-import { P, H1, H2 } from '../../components/Typography';
+import { P, H1, H2, H3 } from '../../components/Typography';
 import { Input, TextArea } from '../../components/Inputs';
 import { useAuth } from '../../contexts/AuthContext';
 import { useLocation } from 'preact-iso'
+
+import Select from 'react-select'
+import CreatableSelect, { useCreatable } from 'react-select/creatable';
 
 export default function New() {
   const [title, setTitle] = useState("");
@@ -13,19 +16,29 @@ export default function New() {
   const [year, setYear] = useState("");
   const [condition, setCondition] = useState(0);
   const [shelves, setShelves] = useState([]);
+  const [selectedAuthors, setSelectedAuthors] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedShelves, setSelectedShelves] = useState([]);
+  const [selectedPublisher, setSelectedPublisher] = useState();
 
+  // Options data and shit
   const [genres, setGenres] = useState([]);
   const [authors, setAuthors] = useState([]);
   const [publishers, setPublishers] = useState([]);
 
+  // For the whole page
   const [loading, setLoading] = useState(false);
+
+  const [loadingAuthors, setLoadingAuthors] = useState(false);
+  const [loadingGenres, setLoadingGenres] = useState(false);
+  const [loadingPublishers, setLoadingPublishers] = useState(false);
 
   const { token } = useAuth();
   const { route } = useLocation();
 
   useEffect(() => {
     const fetchGenres = async () => {
-      setLoading(true);
+      setLoadingGenres(true);
 
       try {
         const res = await fetch("/api/internal/genres", {
@@ -38,12 +51,14 @@ export default function New() {
         if (res.ok) {
           const data = await res.json();
 
-          setGenres(data.genres);
+          setGenres(
+            data.genres.map((g) => ({ label: g.name, value: g.id }))
+          );
         } else {
           console.error("Failed to fetch genres...");
         }
       } finally {
-        setLoading(false);
+        setLoadingGenres(false);
       }
     }
 
@@ -60,7 +75,9 @@ export default function New() {
         if (res.ok) {
           const data = await res.json();
 
-          setGenres(data.shelves);
+          setShelves(
+            data.shelves.map((s) => ({ label: s.name, value: s.id }))
+          );
         } else {
           console.error("Failed to fetch user shelves...");
         }
@@ -70,6 +87,8 @@ export default function New() {
     }
 
     const fetchAuthors = async () => {
+      setLoadingAuthors(true);
+
       try {
         const res = await fetch("/api/internal/authors", {
           headers: {
@@ -81,17 +100,21 @@ export default function New() {
         if (res.ok) {
           const data = await res.json();
 
-          setAuthors(data.authors);
+          setAuthors(
+            data.authors.map((a) => ({ label: a.name, value: a.id }))
+          );
         } else {
           console.error("Failed to fetch authors...");
         }
       } finally {
-        setLoading(false);
+        setLoadingAuthors(false);
       }
     }
 
     // TODO: We could just retrieve from the context?
     const fetchPublishers = async () => {
+      setLoadingPublishers(true);
+
       try {
         const res = await fetch("/api/internal/publishers", {
           headers: {
@@ -103,12 +126,14 @@ export default function New() {
         if (res.ok) {
           const data = await res.json();
 
-          setPublishers(data.publishers);
+          setPublishers(
+            data.publishers.map((p) => ({ label: p.name, value: p.id }))
+          );
         } else {
           console.error("Failed to fetch publishers...");
         }
       } finally {
-        setLoading(false);
+        setLoadingPublishers(false);
       }
     }
 
@@ -120,7 +145,103 @@ export default function New() {
     }
   }, [token]);
 
-  async function handleSubmit(e) {
+  const onCreateAuthor = async (inputString) => {
+    setLoadingAuthors(true);
+
+    try {
+      const res = await fetch("/api/internal/authors", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          author: { name: inputString }
+        })
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        const newAuthor = { value: data.author.id, label: data.author.name };
+
+        setAuthors(
+          (prev) => [...prev, newAuthor]
+        );
+
+        setSelectedAuthors((prev) => [...prev, newAuthor])
+      } else {
+        console.error("Failed to create author");
+      }
+    } finally {
+      setLoadingAuthors(false);
+    }
+  }
+
+  const onCreateGenre = async (inputString) => {
+    setLoadingGenres(true);
+
+    try {
+      const res = await fetch("/api/internal/genres", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          genre: { name: inputString }
+        })
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        const newGenre = { value: data.genre.id, label: data.genre.name };
+
+        setGenres(
+          (prev) => [...prev, newGenre]
+        );
+
+        setSelectedGenres((prev) => [...prev, newGenre])
+      } else {
+        console.error("Failed to create genres");
+      }
+    } finally {
+      setLoadingGenres(false);
+    }
+  }
+
+  const onCreatePublisher = async (inputString) => {
+    setLoadingPublishers(true);
+
+    try {
+      const res = await fetch("/api/internal/publishers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          publisher: { name: inputString }
+        })
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        const newPublisher = { value: data.publisher.id, label: data.publisher.name };
+
+        setPublishers(
+          (prev) => [...prev, newPublisher]
+        );
+
+        setSelectedPublisher(newPublisher)
+      } else {
+        console.error("Failed to create publisher");
+      }
+    } finally {
+      setLoadingPublishers(false);
+    }
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
@@ -133,11 +254,14 @@ export default function New() {
         },
         body: JSON.stringify({
           book: {
-            name,
+            title,
             description,
             year,
             condition,
-            shelves
+            publisher_id: selectedPublisher.value,
+            shelf_ids: selectedShelves.map((s) => s.value),
+            author_ids: selectedAuthors.map((a) => a.value),
+            genre_ids: selectedGenres.map((g) => g.value)
           }
         })
       });
@@ -170,6 +294,7 @@ export default function New() {
             type="text"
             placeholder="Título do Livro"
             value={title}
+            required="true"
             onInput={(e) => setTitle(e.target.value)}
           />
 
@@ -184,15 +309,88 @@ export default function New() {
             placeholder="Ano da Edição desse livro"
             value={year}
             maxlength="4"
+            required="true"
+            min={1}
+            max={new Date().getFullYear()}
             onInput={(e) => setYear(e.target.value)}
           />
+
+          <Select
+            required="true"
+            placeholder="Estante"
+            isMulti
+            onChange={(newValue) => setSelectedShelves(newValue)}
+            value={selectedShelves}
+            options={shelves} />
+
+          <hr className="border-neutral-300" />
+
+          <div className="mb-2">
+            <H2>Informações do Livro</H2>
+
+            <P>
+              Aqui precisamos identificar mais dados que compõe o livro, para
+              relacionar com outros dados para ser indexados
+            </P>
+          </div>
+
+          <fieldset className="mb-1">
+            <H3 className="mb-1">Autores</H3>
+
+            <CreatableSelect
+              required="true"
+              placeholder="Autores"
+              isMulti
+              formatCreateLabel={(newValue) => `Criar autor "${newValue}"`}
+              createOptionPosition="first"
+              onCreateOption={onCreateAuthor}
+              options={authors}
+              isLoading={loadingAuthors}
+              isDisabled={loadingAuthors}
+              onChange={(newValue) => setSelectedAuthors(newValue)}
+              value={selectedAuthors} />
+          </fieldset>
+
+          <fieldset className="mb-1">
+            <H3 className="mb-1">Gêneros</H3>
+
+            <CreatableSelect
+              required="true"
+              placeholder="Gêneros"
+              isMulti
+              formatCreateLabel={(newValue) => `Criar gênero "${newValue}"`}
+              createOptionPosition="first"
+              onCreateOption={onCreateGenre}
+              options={genres}
+              isLoading={loadingGenres}
+              isDisabled={loadingGenres}
+              onChange={(newValue) => setSelectedGenres(newValue)}
+              value={selectedGenres} />
+          </fieldset>
+
+          <fieldset className="mb-1">
+            <H3 className="mb-1">Editora</H3>
+
+            <CreatableSelect
+              isClearable
+              required="true"
+              placeholder="Editora"
+              formatCreateLabel={(newValue) => `Criar editora "${newValue}"`}
+              createOptionPosition="first"
+              onCreateOption={onCreatePublisher}
+              options={publishers}
+              isLoading={loadingPublishers}
+              isDisabled={loadingPublishers}
+              onChange={(newValue) => setSelectedPublisher(newValue)}
+              value={selectedPublisher} />
+          </fieldset>
 
           <Button
             type="submit"
             size="lg"
             disabled={loading}
           >
-            {loading ? "Adicionando..." : "Adicionando livro"}
+            {loading ? "Adicionando..." : "Adicionar Livro"}
           </Button>
         </form>
       </div>
