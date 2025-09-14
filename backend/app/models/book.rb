37242b7518ef25b3e252c0ef22499d6b8bdfbcde
Pedro Lucas Porcellis <porcellis@eletrotupi.com>
2025-09-14 20:48:29 -0300

    backend: tidying missing model relationships

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :publisher
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  has_many :shelf_books
+  has_many :shelves, through: :shelf_books
+
   has_and_belongs_to_many :genres
 
   enum :condition, [:used, :new], default: :used, prefix: :condition
